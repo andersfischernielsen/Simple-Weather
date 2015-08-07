de8e5739c339050832ed8ca0b4c9e8fd9d751e5a
Anders Fischer-Nielsen <andersfischern@me.com>
2015-08-07 12:40:10 +0200

    Intermediary commit to store changes.

--- Simple Weather/Simple Weather/WeatherTableViewController.swift
+++ Simple Weather/Simple Weather/WeatherTableViewController.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import UIKit
+
+class WeatherTableViewController: UITableViewController {
+    
+    let data = [
+        HourWeatherData(icon: "test", time: NSDate(), temperature: 20.0, precipation: nil),
+        HourWeatherData(icon: "test", time: NSDate(), temperature: 20.0, precipation: nil),
+        HourWeatherData(icon: "test", time: NSDate(), temperature: 20.0, precipation: nil),
+        HourWeatherData(icon: "test", time: NSDate(), temperature: 20.0, precipation: nil),
+        HourWeatherData(icon: "test", time: NSDate(), temperature: 20.0, precipation: nil)
+    ]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("WeatherCell", forIndexPath: indexPath) as! WeatherTableViewCell
+        let entry = data[indexPath.row]
+        
+        cell.iconImage.image = UIImage(named: entry.icon)
+        cell.timeLabel.text = entry.time
+        cell.tempLabel.text = entry.temperature
+        
+        //times[i].text = getPrintDate(data[i].time)
+        //images[i].image = UIImage(named: forecast.icon)
+        //temps[i].text = String(format: "%.1f", forecast.temperature) + " °C"
+        //constraints[i].constant = CGFloat(200 * forecast.precipation)
+        
+        return cell
+    }
+}
+
+class WeatherTableViewCell: UITableViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+}
