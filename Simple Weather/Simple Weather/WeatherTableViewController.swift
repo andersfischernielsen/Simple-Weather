cfb02c6ac3dd990605a26790bf7b30ab965703c3
Anders Fischer-Nielsen <andersfischern@me.com>
2015-08-07 12:39:41 +0200

    Added tableviewcontroller for controlling inner table.

---
+++ Simple Weather/Simple Weather/WeatherTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherTableViewController.swift
+//  Simple Weather
+//
+//  Created by Anders Fischer-Nielsen on 07/08/15.
+//  Copyright (c) 2015 Anders Fischer-Nielsen. All rights reserved.
+//
+
+import Foundation
